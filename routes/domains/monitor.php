d73661a18175860fb0b0bd7939b0a77ab32bcefd
Michel Cantagalo <drmichelcantagalo@gmail.com>
2025-11-06 17:24:21 +0100

    debug

--- routes/domains/monitor.php
+++ routes/domains/monitor.php
@@ -42,7 +42,7 @@ Route::middleware(['avoid.robots'])->group(function () {
             App::setLocale($lang);
         }
         if(session('avoid_monitor')) { session()->forget('avoid_monitor'); }
-        //if (!session('show_cookie')) { return view('fallback'); }
+        if (!session('show_cookie')) { return view('fallback'); }
         //else {
             //session()->forget('show_cookie');
             return view('popups.cookies')->with(['lang' => $lang, 'domain' => 'monitor', 'debug' => json_encode(session()->all())]);

55f15b2a6822bdcbb5b0ea11cc11a3e36dc27997
Michel Cantagalo <drmichelcantagalo@gmail.com>
2025-11-07 12:01:38 +0100

    Api service added

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\APIService;
 
 class APIController extends Controller
 {
-    //
+    protected $handler;
+    
+    public function __construct(APIService $handler) {
+        $this->handler = $handler;
+    }
 }

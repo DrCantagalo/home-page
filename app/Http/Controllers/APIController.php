8c0cb6416a116f94bd4cd1ed52eddc75b38e64c5
Michel Cantagalo <drmichelcantagalo@gmail.com>
2025-11-07 14:47:00 +0100

    registerinstallation logic start

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -12,4 +12,16 @@ class APIController extends Controller
     public function __construct(APIService $handler) {
         $this->handler = $handler;
     }
+
+    public function registerinstallation(Request $request){
+        
+        $request->validate([
+            'site_url' => 'required|url',
+            'package_version' => 'required|string',
+            'installation_hash' => 'required|string',
+        ]);
+
+        return $this->handler->registerinstallation($request);
+
+    }
 }
